b8d8278aeb26321d68fcac3bc6172746ada0fa11
jv <jv88899@gmail.com>
2018-04-14 11:47:52 -0500

    make revisions after first review

--- src/App.js
+++ src/App.js
@@ -12,35 +12,36 @@ class App extends React.Component {
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
-        this.setState({
-          books: books
-        })
+        this.setState({ books })
       })
   }
 
   handleChange = (book, newShelf) => {
     BooksAPI.update(book, newShelf).then(() => {
-      BooksAPI.getAll().then((books) => {
-        this.setState({
-          books: books
-        })
+      book.shelf = newShelf;
+      let newBooks = this.state.books.filter( (newBook) => {
+        return newBook.id !== book.id;
       })
-    });
+      // newBooks.push(book);
+      this.setState({ books: newBooks.concat(book) })
+    })
   }
 
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
         <Route exact path="/" render={() => (
           <BookShelf
-            books={this.state.books}
+            books={books}
             handleChange={this.handleChange}
           />
         )} />
         <Route path="/search" render={() => (
           <SearchPage
             handleChange={this.handleChange}
-            shelf={this.state.books}
+            shelf={books}
           />
         )} />
       </div>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,44 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Shelf from './Shelf';
 
-class BookShelf extends React.Component {
-    render() {
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <Shelf
-                            shelfTitle={'Currently Reading'}
-                            books={this.props.books.filter( (book) => {
-                                return book.shelf === 'currentlyReading'
-                            })}
-                            handleChange={this.props.handleChange}
-                        />
-                        <Shelf
-                            shelfTitle={'Want to Read'}
-                            books={this.props.books.filter( (book) => {
-                                return book.shelf === 'wantToRead'
-                            })}
-                            handleChange={this.props.handleChange}
-                        />
-                        <Shelf
-                            shelfTitle={'Read'}
-                            books={this.props.books.filter( (book) => {
-                                return book.shelf === 'read'
-                            })}
-                            handleChange={this.props.handleChange}
-                        />
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add a book</Link>
-                </div>
+const BookShelf = (props) => (
+    <div className="list-books">
+        <div className="list-books-title">
+            <h1>My Reads</h1>
+        </div>
+        <div className="list-books-content">
+            <div>
+                <Shelf
+                    shelfTitle={'Currently Reading'}
+                    books={props.books.filter( (book) => {
+                        return book.shelf === 'currentlyReading'
+                    })}
+                    handleChange={props.handleChange}
+                />
+                <Shelf
+                    shelfTitle={'Want to Read'}
+                    books={props.books.filter( (book) => {
+                        return book.shelf === 'wantToRead'
+                    })}
+                    handleChange={props.handleChange}
+                />
+                <Shelf
+                    shelfTitle={'Read'}
+                    books={props.books.filter( (book) => {
+                        return book.shelf === 'wantToRead'
+                    })}
+                    handleChange={props.handleChange}
+                />
             </div>
-        )
-    }
-}
+        </div>
+        <div className="open-search">
+            <Link to='/search'>Add a Book</Link>
+        </div>
+    </div>
+)
 
 export default BookShelf;
